d7b4cd94a884cd4b3ea6262949ca4489cb8af639
Obasi Ngozi <ndukaubangozi@gmail.com>
2023-11-27 12:15:04 +0100

    task

---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+number = 3.14159
+new_number = round(number,2)
+print(f"float: {new_number}\n")
+
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Holberton School" *3
+print(f"{str}\n"+ "Holberton\n")
+
