edd218704b968309a23a528fe26410a0de063d46
Obasi Ngozi <ndukaubangozi@gmail.com>
2023-12-06 15:06:09 +0100

    task3

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -2,13 +2,7 @@
 
 
 def number_keys(a_dictionary):
-    """ function that returns the number of keys in a dictionary.
-
-        Args:
-        a_dictionary: a dictionary
-
-        Return:
-        the number of keys
     """
-    return (len(a_dictionary.keys()))
-
+    A function that returns the number of keys in a dictionary.
+    """
+    return len(a_dictionary)
