77b92ea45952fd79c2d18ba355562a91e72c244a
Obasi Ngozi <ndukaubangozi@gmail.com>
2023-12-06 14:53:58 +0100

    taskk

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary):
+    """ function that returns the number of keys in a dictionary.
+
+        Args:
+        a_dictionary: a dictionary
+
+        Return:
+        the number of keys
+    """
+    return (len(a_dictionary.keys()))
+
--- 0x04-python-more_data_structures/lists.h
+++ 0x04-python-more_data_structures/lists.h
@@ -2,4 +2,15 @@
 #define LIST_H
 
 def square_matrix_simple(matrix=[]):
+def search_replace(my_list, search, replace):
+def uniq_add(my_list=[]):
+def common_elements(set_1, set_2):
+def only_diff_elements(set_1, set_2):
+def number_keys(a_dictionary):
+def print_sorted_dictionary(a_dictionary):
+def update_dictionary(a_dictionary, key, value):
+def simple_delete(a_dictionary, key=""):
+def multiply_by_2(a_dictionary):
+def best_score(a_dictionary):
 #endif
+
