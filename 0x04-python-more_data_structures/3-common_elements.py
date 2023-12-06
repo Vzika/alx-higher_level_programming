a9f11cc9d9e9afd407de60fa4399c5c1576c697c
Obasi Ngozi <ndukaubangozi@gmail.com>
2023-12-06 15:03:24 +0100

    task3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,8 +2,13 @@
 
 
 def common_elements(set_1, set_2):
+    """ function that returns a set of common elements in two sets.
+
+        Args:
+        set_1: first set
+        set_2: second set
+
+        Return:
+        returns a set of common elements in two sets
     """
-    A function that returns a set of
-    common elements in two sets
-    """
-    return(set_1 & set_2)
+    return (set_1 & set_2)
