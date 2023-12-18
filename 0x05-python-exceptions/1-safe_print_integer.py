de2fc4924f260448e70d4de459ed4b4c6204a5c4
Obasi Ngozi <ndukaubangozi@gmail.com>
2023-12-18 14:01:07 +0100

    task1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-	try:
-		print("{:d}".format(value))
-		return True
-	except (ValueError,TypeError):
-		return False
+    """safe_print_integer - prints an integer with "{:d}".format().
+
+    Args:
+        value: value can be any type
+
+    Return:
+        True if value has been correctly printed
+        (it means the value is an integer)
+        Otherwise, returns False
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
