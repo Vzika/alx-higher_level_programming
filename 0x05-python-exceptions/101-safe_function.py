af1b4951e9f1f31d4a06c6deb89e4e335617dd78
Obasi Ngozi <ndukaubangozi@gmail.com>
2023-12-18 21:54:00 +0100

    task

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
-    """safe_function - function that execute a function safely
-
-    Args:
-        fct: will always be a pointer to a function
-        *args: a tuple of positional arguments
-
-    Return:
-        result of the function
-    """
-    from sys import stderr
+    import sys
     try:
-        return (fct(*args))
-    except Exception as err:
-        print("Exception: {}".format(err), file=stderr)
+        result = fct(*args)
+    except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
+        result = None
+
+    return (result)
 
