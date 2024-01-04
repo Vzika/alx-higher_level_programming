c45223768dee715b5781cbf4e524c647c80362cb
Obasi Ngozi <ndukaubangozi@gmail.com>
2024-01-04 23:37:49 +0100

    task0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+0-rectangle.py module - contains Rectangle() class
+"""
+
+
+class Rectangle():
+    """Rectangle - mpty class Rectangle that defines a rectangle"""
+    pass
+
