a89080f5baa9cc468656ecc866afefcece1d3abf
Obasi Ngozi <ndukaubangozi@gmail.com>
2024-01-08 15:16:16 +0100

    101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""101-add_attribute module - add attribute
-"""
+"""Defines a function that adds attributes to objects."""
 
 
-def add_attribute(obj=object, attr=str, value=str):
-    """dynamically set a new attribute to a class"""
-    if not isinstance(obj, object):
+def add_attribute(obj, att, value):
+    """Add a new attribute to an object if possible.
+    Args:
+        obj (any): The object to add an attribute to.
+        att (str): The name of the attribute to add to obj.
+        value (any): The value of att.
+    Raises:
+        TypeError: If the attribute cannot be added.
+    """
+    if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
-    for att in dir(obj):
-        if att[2:-2] is attr:
-            raise TypeError("can't add new attribute")
-    obj.attr = value
+    setattr(obj, att, value)
