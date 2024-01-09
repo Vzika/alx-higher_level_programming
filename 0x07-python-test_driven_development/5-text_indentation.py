d1cbfe95662a506fd9eb3684d6d02ccfeb944ac3
Obasi Ngozi <ndukaubangozi@gmail.com>
2024-01-09 22:25:13 +0100

    task5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""Text indentation module
-
-Test file for this module is located in ./tests/ directory
-Run (python3 -m doctest -v ./tests/5-text_indentation.txt | tail -2)
-to test remove the pipeline to see full details"""
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    """text_indentation - prints a text with 2 new lines
-    after each of these characters: ., ? and :
+    """Print text with two new lines after each '.', '?', and ':'.
 
     Args:
-        text:must be a string
-
-    Return:
-        Nothing
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
     """
     if not isinstance(text, str):
-        raise TypeError('text must be a string')
-    _text = text.replace('.', '\n\n').replace('?', '\n\n').replace(':', '\n\n')
-    for _ in range(len(text)):
-        _text = _text.replace('\n ', '\n')
-    print(_text, end='')
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
