212f60d0e6196416f1ae185282c49e3a51e3d67a
Obasi Ngozi <ndukaubangozi@gmail.com>
2024-01-09 22:15:25 +0100

    task2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""Defines an integer addition function."""
+"""This module contains:
+add_integer - function that adds up two integers.
+
+Test file for this module is located in ./tests/ directory
+Run (python3 -m doctest -v ./tests/0-add_integer.txt) to test
+"""
 
 
 def add_integer(a, b=98):
-    """Return the integer addition of a and b.
+    """add_integer - adds 2 integers
 
-    Float arguments are typecasted to ints before addition is performed.
+    Args:
+        a: positional argument a (must be an integer)
+        b: keyword argumrnt b (must be an integer)
 
-    Raises:
-        TypeError: If either of a or b is a non-integer and non-float.
+    Return:
+        sum of two integers or the keyword value if one arg is given
     """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
-        raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
-        raise TypeError("b must be an integer")
-    return (int(a) + int(b))
-
+    if not isinstance(a, (int, float)):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, (int, float)):
+        raise TypeError('b must be an integer')
+    return int(a + b)
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
+"""function that divides all elements of a matrix
+
+Test file for this module is located in ./tests/ directory
+Run (python3 -m doctest -v ./tests/2-matrix_divided.txt) to test
+"""
+
+
 def matrix_divided(matrix, div):
     """matrix_divided -  divides all elements of a matrix
 
@@ -6,36 +14,25 @@ def matrix_divided(matrix, div):
         div: number (integer or float)
 
     Return:
-        a new matrix with elements divided by div and rounded to 2 decimal places
+        a new matrix
     """
 
     if not isinstance(matrix, list):
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-
+        raise TypeError('matrix must be a matrix \
+(list of lists) of integers/floats')
     for i in range(len(matrix)):
         if not isinstance(matrix[i], list):
-            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-
+            raise TypeError('matrix must be a matrix \
+(list of lists) of integers/floats')
         for each_int in matrix[i]:
             if not isinstance(each_int, (int, float)):
-                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-
-        if i < len(matrix) - 1 and len(matrix[i]) != len(matrix[i + 1]):
+                raise TypeError('matrix must be a matrix \
+(list of lists) of integers/floats')
+        if len(matrix) != i + 1 and len(matrix[i]) != len(matrix[i + 1]):
             raise TypeError('Each row of the matrix must have the same size')
-
     if not isinstance(div, (int, float)):
         raise TypeError('div must be a number')
-
     if div == 0:
         raise ZeroDivisionError('division by zero')
 
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for element in row:
-            result = round(element / div, 2)
-            new_row.append(result)
-        new_matrix.append(new_row)
-
-    return new_matrix
-
+    return [[round(_int / div, 2) for _int in _item] for _item in matrix]
