f8c7430aef6839275f28da3201c26138856c3834
Obasi Ngozi <ndukaubangozi@gmail.com>
2024-03-11 14:22:50 +0100

    javascrip_warm_up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,21 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args.length === 0) {
+/*
+ * script that prints the first argument passed to it:
+ * If no arguments are passed to the script, print “No argument”
+ * You must use console.log(...) to print all output
+ * You are not allowed to use var
+ * You are not allowed to use length
+ */
+
+// const args = process.argv.slice(2);
+// if (args.length === 0) {
+//   console.log('No argument');
+// } else {
+//   console.log(args[0]);
+// }
+
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
-  console.log(args[0]);
+  console.log(process.argv[2]);
 }
