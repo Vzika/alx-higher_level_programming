9e27f3af94628d13aacbbe94233048c52f600977
Obasi Ngozi <ndukaubangozi@gmail.com>
2024-03-11 20:23:46 +0100

    js_wm_up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,21 +1,2 @@
 #!/usr/bin/node
-/*
- * script that prints the first argument passed to it:
- * If no arguments are passed to the script, print “No argument”
- * You must use console.log(...) to print all output
- * You are not allowed to use var
- * You are not allowed to use length
- */
-
-// const args = process.argv.slice(2);
-// if (args.length === 0) {
-//   console.log('No argument');
-// } else {
-//   console.log(args[0]);
-// }
-
-if (process.argv[2] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-}
+console.log(process.argv[2] ? process.argv[2] : 'No argument');
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,14 +1,2 @@
 #!/usr/bin/node
-/*
- * script that prints `My number: <first argument converted in integer>` if the first argument can be converted to an integer:
- * If the argument can’t be converted to an integer, print “Not a number”
- * You must use console.log(...) to print all output
- * You are not allowed to use var
- * You are not allowed to use try/catch
- */
-const args = process.argv.slice(2);
-if (isNaN(parseInt((args[0]), 10))) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' +  parseInt((args[0]), 10));
-}
+console.log(parseInt(process.argv[2]) ? `My number: ${parseInt(process.argv[2])}` : 'Not a number');
