569521ccceed5b00ff79c3ad32544b947f59ca23
Obasi Ngozi <ndukaubangozi@gmail.com>
2024-03-17 14:49:50 +0100

    orm

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa
+Use (source maintest/0-select_states.sql) to load into the MySQL Server
+Execute by (./0-select_states.py username password database_name)
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    conn = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+    )
+    cursor = conn.cursor()
+    query = 'SELECT * FROM states ORDER BY id ASC'
+    cursor.execute(query)
+    results = cursor.fetchall()
+    
+    for row in results:
+    print(row)
+    
+    cursor.close()
+    conn.close()
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa
+Use (source maintest/0-select_states.sql) to load into the MySQL Server
+Execute by (./0-select_states.py username password database_name)
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    conn = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
+    )
+    cursor = c onn.cursor()
+    query = ('SELECT * FROM states '
+            'WHERE states.name LIKE BINARY "N%" '
+            'ORDER BY states.id ASC')
+    cursor.execute(query)
+    results = cursor.fetchall()
+    
+    for row in results:
+    print(row)
+    
+    cursor.close()
+    conn.close()
