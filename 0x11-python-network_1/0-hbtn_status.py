2f3828b8631b46fc91c6c5ecf1db69032027115c
Obasi Ngozi <ndukaubangozi@gmail.com>
2024-03-28 15:57:02 +0100

    netwok1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     import urllib.request as request
     with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         if response:
-            html = response.read()
+            content = response.read()
             print('Body response:')
-            print(f"\t- type: {type(html)}")
-            print(f"\t- content: {html}")
-            print(f"\t- utf8 content: {html.decode('utf-8')}")
+            print(f"\t- type: {type(content)}")
+            print(f"\t- content: {content}")
+            print(f"\t- utf8 content: {content.decode('utf-8')}")
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
+"""script that takes in a URL, sends a request to the URL and displays the
+value of the X-Request-Id variable found in the header of the response.
 """
-import sys
-import urllib.request
 
 if __name__ == "__main__":
-	url = sys.argv[1]
+    import urllib.request as request
+    from sys import argv
 
-	request = urllib.request.Request(url)
-	with urllib.request.urlopen(request) as response:
-		header_id = response.headers.get('X-Request-Id')
-		print(header_id);
+    with request.urlopen(argv[1]) as response:
+        header_id = response.headers.get('X-Request-Id')
+        print(header_id)
